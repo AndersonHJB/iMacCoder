ddd7940f7cea4481b5d6f96feb957f9a39c29d6c
iMac-Aiyuechuang <1432803776@qq.com>
2022-01-10 20:51:44 +0800

    update

---
+++ VisualCode/lesson01.py
@@ -0,0 +1,60 @@
+print = "aiyc"
+
+# user_name = "aiyc"
+# user_name = "aiyc"
+# user_name = "aiyc"
+# user_name = "aiyc"
+# user_name = "aiyc"
+# user_name = "aiyc"
+# user_name = "aiyc"
+
+# print(help("keywords"))
+# aS = 1
+# na01me01 = "aiyuechuang"
+# na01me01 = "aiyuechuang"
+# na01me01 = "aiyuechuang"
+
+# a = "可乐"
+# b = "橙汁"
+# 不添加第三个变量呢？
+# c = a
+# a = b
+# b = c
+# a, b = b, a
+# print("a:", a)
+# print("b:", b)
+
+# a = 5
+# b = 10
+# c = 15
+# a, b, c = 5, 10, 15
+# print("a 的值是：", a)
+# print("b 的值是：", b)
+# print("c 的值是：", c)
+#
+# print("a", a, "b", b, "c", c)
+
+
+# a = 1
+# b = 1
+# c = 1
+# a = b = c = 1
+# print("a 的值是：", a)
+# print("b 的值是：", b)
+# print("c 的值是：", c)
+#
+# print("a", a, "b", b, "c", c)
+
+
+# x = 1
+# x = x + 10
+# print(x)
+# 注释、注解 control + /，#
+
+# name1 = "lilei"
+# name2 = name1
+# print(name2)
+
+# name1 = "lilei"
+# name1 = "hanmeimei"
+# print(name1)
---
+++ student_code/student2.py
@@ -0,0 +1,38 @@
+a = 1
+# print(type(a))
+type1 = type(a)
+print(type1)
+
+b = 1.0
+print(type(b))
+
+str1 = "aiyuechuang12"
+print(str1)
+print(type(str1))
+
+lst = [1, 2, 3, "aiyc", 1.1]
+print(lst)
+
+# 错误代码
+# name = "aiyc"
+# fujian = "福建"
+# lst = {1, 2, name, fujian}
+
+
+dict1 = {"name": "aiyuechuang", "age": 18}
+# key: 要求使用不可变的数据类型
+# value: 任意数据类型
+# 无序
+print(dict1)
+
+set1 = {1, 2, 3, 4, "aiyc"}
+# Question1：set 能放进 set 么？
+# 确定性：确定
+# 互异性：不能重复
+# 无序性：没有顺序
+
+
+
+
+
+
